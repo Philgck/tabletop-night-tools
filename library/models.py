1681b9beb618832384a210d61ee0fc383822236e
Philip Kershaw <pgckershaw@gmail.com>
2025-01-16 14:39:02 +0000

    Add: Rudimentary AddGame model, ability for admin to add games list

---
+++ library/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+# Boardgame List Model. The intent of this model is to update the database with a personalised list of games. 
+
+class AddGame(models.Model):
+    title = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    minimum_player_count = models.IntegerField()
+    maximum_player_count = models.IntegerField()
