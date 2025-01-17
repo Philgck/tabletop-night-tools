acfc9d4338e4107bafdbe319c9169078f0e4baf5
Philip Kershaw <pgckershaw@gmail.com>
2025-01-17 11:06:08 +0000

    Add: Basic test view to model.

--- library/views.py
+++ library/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import AddGame
 
 # Create your views here.
+class LibraryList(generic.ListView):
+    queryset = AddGame.objects.all()
+    template_name = "addgame_list.html"
--- tabletop_night_tools/urls.py
+++ tabletop_night_tools/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home import views as index_views
+from django.urls import path, include
+from library import views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("library.urls"), name="library-urls"),
     path('admin/', admin.site.urls),
 ]
