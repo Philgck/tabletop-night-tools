7b9de3fcc2b09302e6e79fb68bead196295ade15
Philip Kershaw <pgckershaw@gmail.com>
2025-01-22 16:17:04 +0000

    search bgg script moved into static

---
+++ library/static/js/searchBGG.js
@@ -0,0 +1,38 @@
+// This script is used to search for games on BoardGameGeek and populate the game form with the selected game's details
+
+    async function searchBGGGame() {
+        const gameName = document.getElementById('gameSearch').value;
+        if (gameName) {
+            const response = await fetch(`/search_bgg_games/?game_name=${encodeURIComponent(gameName)}`);
+            if (response.ok) {
+                const games = await response.json();
+                populateGameOptions(games);
+            } else {
+                alert('No games found');
+            }
+        }
+    }
+
+    function populateGameOptions(games) {
+        const gameSelect = document.getElementById('gameSelect');
+        gameSelect.innerHTML = '<option value="">Select a game</option>';
+        games.forEach(game => {
+            const option = document.createElement('option');
+            option.value = JSON.stringify(game);
+            option.textContent = game.name;
+            gameSelect.appendChild(option);
+        });
+    }
+
+    function selectGame() {
+        const gameSelect = document.getElementById('gameSelect');
+        const selectedGame = gameSelect.value;
+        if (selectedGame) {
+            const game = JSON.parse(selectedGame);
+            document.getElementById('id_title').value = game.name;
+            document.getElementById('id_description').value = game.description;
+            document.getElementById('id_minimum_player_count').value = game.minplayers;
+            document.getElementById('id_maximum_player_count').value = game.maxplayers;
+            document.getElementById('id_image_url').value = game.image;
+        }
+    }
--- staticfiles/js/searchBGG.js
+++ staticfiles/js/searchBGG.js
@@ -1,22 +1,38 @@
-// function searchGame() {
-//     var gameName = document.getElementById('gameSearch').value;
-    
-//     if (gameName === "") {
-//         alert("Please enter a game name.");
-//         return;
-//     }
+// This script is used to search for games on BoardGameGeek and populate the game form with the selected game's details
 
-//     var apiUrl = `https://www.boardgamegeek.com/xmlapi2/search?search=${encodeURIComponent(gameName)}&type=boardgame`;
+async function searchBGGGame() {
+    const gameName = document.getElementById('gameSearch').value;
+    if (gameName) {
+        const response = await fetch(`/search_bgg_games/?game_name=${encodeURIComponent(gameName)}`);
+        if (response.ok) {
+            const games = await response.json();
+            populateGameOptions(games);
+        } else {
+            alert('No games found');
+        }
+    }
+}
 
-//     fetch(apiUrl)
-//         .then(response => response.text())
-//         .then(data => {
-//             var parser = new DOMParser();
-//             var xmlDoc = parser.parseFromString(data, "text/xml");
-//             displayGameInfo(xmlDoc);
-//         })
-//         .catch(error => {
-//             console.error("Error fetching game data: ", error);
-//             alert("Something went wrong. Please try again.");
-//         });
-// }
+function populateGameOptions(games) {
+    const gameSelect = document.getElementById('gameSelect');
+    gameSelect.innerHTML = '<option value="">Select a game</option>';
+    games.forEach(game => {
+        const option = document.createElement('option');
+        option.value = JSON.stringify(game);
+        option.textContent = game.name;
+        gameSelect.appendChild(option);
+    });
+}
+
+function selectGame() {
+    const gameSelect = document.getElementById('gameSelect');
+    const selectedGame = gameSelect.value;
+    if (selectedGame) {
+        const game = JSON.parse(selectedGame);
+        document.getElementById('id_title').value = game.name;
+        document.getElementById('id_description').value = game.description;
+        document.getElementById('id_minimum_player_count').value = game.minplayers;
+        document.getElementById('id_maximum_player_count').value = game.maxplayers;
+        document.getElementById('id_image_url').value = game.image;
+    }
+}
