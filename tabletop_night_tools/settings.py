82997a7dd014f08bc04542dd07b80484709dd317
Philip Kershaw <pgckershaw@gmail.com>
2025-01-23 11:38:17 +0000

    Switch debug back to false as it did not reveal what is preventing deployment

--- tabletop_night_tools/settings.py
+++ tabletop_night_tools/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-ktm5aqo9bm_k=+5lpy)mfixgqy82n1hljdl72og$w7$67j5a*a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', ]
 
