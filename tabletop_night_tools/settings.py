0cc4db5633fd804ba317c0d86de2fa4c7e05180e
Philip Kershaw <pgckershaw@gmail.com>
2025-01-27 15:01:37 +0000

    Add: Dummy social account links to support page

--- tabletop_night_tools/settings.py
+++ tabletop_night_tools/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', 'tabletop-night-tools-5e7ac4f69b28.herokuapp.com',]
 
